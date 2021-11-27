705f91addd38260d02c6483bc36ca3b50ee00365
sasank <sasankkathera32@gmail.com>
2021-11-27 12:30:01 -0500

    Verifying type of OS and installing tomcat

--- automate.sh
+++ automate.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+if [ -f /etc/redhat-release ];
+then 
+        yum install java -y
+
+else
+  if [ -f  /etc/debian_version ];
+then 
+        apt install openjdk-11-dbg
+fi
+fi
 
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.55/bin/apache-tomcat-9.0.55.tar.gz
 
