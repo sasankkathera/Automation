0a6e04ee67927b9d54598c1025f482fb2b7b1395
sasankkathera <sasankkathera2@gmail.com>
2021-12-15 16:48:21 +0530

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -19,7 +19,7 @@ fi
 fi
 fi
 
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.55/bin/apache-tomcat-9.0.55.tar.gz
+https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.55/bin/apache-tomcat-9.0.55.tar.gz
 
 tar -xvf apache-tomcat-9.0.55.tar.gz
 
